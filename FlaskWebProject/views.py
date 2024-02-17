44b0cb441c8a33e89ea84aad6fc95dede9b0e47a
Shine <26shiine@gmail.com>
2024-02-17 16:46:47 +0700

    update log message

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -72,7 +72,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
 
-        app.logger.warning('User logged in successfully')
+        app.logger.info(f"User {form.username.data} logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
